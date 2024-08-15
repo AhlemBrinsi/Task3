2e811eb134cc85402cd67d4fb3ed89d17f78c137
Ahlem Brinsi <158343226+AhlemBrinsi@users.noreply.github.com>
2024-08-15 13:09:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+// script.js
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let isGameActive = true;
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+const statusDisplay = document.getElementById('status');
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (board[clickedCellIndex] !== '' || !isGameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    board[clickedCellIndex] = currentPlayer;
+    clickedCell.textContent = currentPlayer;
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = board[winCondition[0]];
+        let b = board[winCondition[1]];
+        let c = board[winCondition[2]];
+
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        isGameActive = false;
+        return;
+    }
+
+    let roundDraw = !board.includes('');
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        isGameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleRestartGame() {
+    isGameActive = true;
+    currentPlayer = 'X';
+    board = ['', '', '', '', '', '', '', '', ''];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.getElementById('resetButton').addEventListener('click', handleRestartGame);
+
+statusDisplay.innerHTML = currentPlayerTurn();
